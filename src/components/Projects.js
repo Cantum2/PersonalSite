06c09933b6e294a922f3fa54d5a24fd019537cf9
Marcus Cantu <cantumarcus19@gmail.com>
2018-10-13 15:36:59 -0600

    Added project comp

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import Project from './components/Project.js'
 class Projects extends Component {
   render() {
     return (
       <div>
         <h1>Projects</h1>
+        <Project />
       </div>
     )
   }

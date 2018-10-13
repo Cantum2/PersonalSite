ef16a6835a26f3db72d7cbc85c70a9c7e592283e
Marcus Cantu <cantumarcus19@gmail.com>
2018-10-13 12:15:41 -0600

    Added dependencies, webpack and babel config

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Stuff</h1>
+            </div>
+        )
+    }
+}
+
+export default App;

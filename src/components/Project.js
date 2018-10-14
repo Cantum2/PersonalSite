abdb54e06db0c6aabd1a1dd9197ba5ae047a831e
Marcus <cantumarcus19@gmail.com>
2018-10-14 13:40:57 -0600

    Added more styles and detail to projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
+import ProjectsCard from '../components/ProjectCard.js';
+import ProjectCardDetail from '../components/ProjectCardDetail';
+let projectsArr = [
+  {
+    name: "Tapt",
+    platform: "Android",
+    technologiesUsed: "Java, Android SDK",
+    description: "Tapt was a game for Android that the goal was to click a ball that randomly moves around the screen. Upon clicking the user was awarded points which could be spent in the in game store."
+  },
+  {
+    name: "Fitter",
+    platform: "Web Applicaiton",
+    technologiesUsed: "AngularJS, NodeJS, Express, Firebase",
+    description: "Social media platform"
+  },
+  {
+    name: "WyoHackathon 2018",
+    platform: "Web Application and Blockchain",
+    technologiesUsed: "AngularJS, TCR, TCR-UI, Git, NodeJS, Express ",
+    description: "Social media platform"
+  }
+];
 
  class Project extends Component {
   render() {
     return (
       <div>
         <p>From project</p>
+        {projectsArr.map(proj => { 
+          return (
+            <ProjectsCard key={proj.name}>
+              <ProjectCardDetail>
+                {proj.name}
+                {proj.platform}
+                Technologies Used: {proj.technologiesUsed}
+                {proj.description}
+              </ProjectCardDetail>
+            </ProjectsCard>
+          )
+        })}
+      
       </div>
     )
   }
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+width: 250px;
+height: 400px;
+border-radius: 25px;
+background-color: red;
+display: inline-block;
+margin-left: 10px;
+box-shadow: 5px 10px #888888;
+`

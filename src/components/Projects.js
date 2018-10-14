ca55594a13ec068c9503ed29db6ab5221a639042
Marcus <cantumarcus19@gmail.com>
2018-10-13 23:19:15 -0600

    Began working on the projects implementation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 import Project from '../components/Project.js'
+let projectsArr = [
+  {
+    name: "Tapt",
+    platform: "Android",
+    technologiesUsed: "Java, Android SDK",
+    description: "Tapt was a game for Android that the goal was to click a ball that randomly moves around the screen. Upon clicking the user was awarded points which could be spent in the in game store."
+  },
+  {
+    name: "Fitter",
+    platform: "Web Applicaiton",
+    technologiesUsed: "AngularJS, NodeJS, Express, Firebase",
+    description: "Social media platform"
+  }
+];
 
 class Projects extends Component {
+  
   render() {
+    
+    const currentProjects = projectsArr.map((proj) => 
+    <li>{proj.name}</li>
+    )
+
     return (
       <div>
         <h1>Projects</h1>
+        <ul>
+          {currentProjects}
+        </ul>
         <Project />
       </div>
     )

d95408e2838a4d0033bb94ee9f19b47750b7ab2c
Marcus Cantu <cantumarcus19@gmail.com>
2018-10-19 21:35:53 -0600

    added project insight

---
+++ src/components/ProjectInsight.js
@@ -0,0 +1,13 @@
+import {React, Component} from "react";
+
+class ProjectInsight extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Test</h1>
+            </div>
+        )
+    }
+};
+
+export default ProjectInsight;
